d680b0d15369957f954770d365d5343a73c5a681
zearakun <91083604+zearakun@users.noreply.github.com>
2022-03-12 22:28:39 +0900

    Update tukaikata.php

--- tukaikata.php
+++ tukaikata.php
@@ -1,1 +1,183 @@
+<html lang="ja">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,600,600italic" />
+<head>
+<meta charset="UTF-8">
+<title>news</title>
+<link rel="icon" type="image/png" href="https://www.pngall.com/wp-content/uploads/4/Cyber-Security-Logo-PNG.png">
+<style>
+body {
+  text-align:center;
+  background-image: url("https://github.com/zearakun/zearaprogram/blob/main/back.png?raw=true");
+  background-size: cover;
+  overflow-y: scroll; 
+     }
 
+body::-webkit-scrollbar {
+ width: 10px;
+ height: 10px;
+  }
+
+.body::-webkit-scrollbar-track {
+ border-radius: 5px;
+ background: rgba(110, 108, 108, 0.2);
+}
+  
+/* ドラックできるスクロール部の設定 */
+.body::-webkit-scrollbar-thumb {
+ border-radius: 5px;
+ background: #FF0000;
+}
+  
+h3 {
+  padding: 1rem 2rem;
+  border-left: 5px solid #000;
+  background: #f4f4f4;
+  margin: auto;
+  text-align: center;
+  width: 50%;
+}
+  
+  
+h1 {
+  padding: 0.4em 0.5em;/*文字の上下 左右の余白*/
+  color: #494949;/*文字色*/
+  background: #f4f4f4;/*背景色*/
+  border-left: solid 5px #7db4e6;/*左線*/
+  border-bottom: solid 3px #d7d7d7;/*下線*/
+  width: 40%;
+  text-align: center;
+  margin: auto;
+  
+}
+  
+  
+  
+nav{
+  width: 100%;
+  height: 70px;
+  background-color: dimgray;
+  padding-top: 5px;
+  box-sizing: border-box;
+  justify-content: center;
+  display: flex
+}
+ul{
+  display: flex;
+}
+li{
+  list-style: none;
+  line-height: 2;
+  display: inline-block;
+}
+nav li a{
+  display: block;
+  text-decoration: none;
+  color: white;
+  margin-right: 35px;
+}
+  
+  
+  
+
+
+ 
+a {
+  position: relative;
+  display: inline-block;
+  text-decoration: none;
+}
+a::after {
+  position: absolute;
+  bottom: -4px;
+  left: 0;
+  content: '';
+  width: 100%;
+  height: 2px;
+  background: #333;
+  transform: scale(0, 1);
+  transform-origin: left top;
+  transition: transform .3s;
+}
+a:hover::after {
+  transform: scale(1, 1);
+}
+  
+  
+  
+  
+  
+
+  
+  
+  
+  </style>
+  </head>
+  
+<body>
+<nav>
+  <ul>
+    <li><a href="command.php" class="aaa">コマンド紹介</a></li>
+    <li><a href="news.php" class="aaa">最新情報</a></li>
+    <li><a href="index.php" class="aaa">HOME</a></li>
+    <li><a href="kiyaku.php" class="aaa">botの規約</a></li>
+    <li><a href="tukaikata.php" class="aaa">botの使い方など</a></li>
+  </ul>
+</nav>
+
+
+
+
+<h3>
+prefixはsc!です<br>
+sc!helpを使ってください
+securityglobalチャンネルを作ってください(globalchat)
+<a href="https://twitter.com/intent/tweet?hashtags=@Za_pythonのpython生活,Za,zeara,zearachat&text=@Za_pythonさんが作ったそうです!!!&url=https://zearachat.herokuapp.com/signup_user_complete/?id=d7kbsjsbstn5xxr6abhec36o7a" target="_blank">ツイートする</a>
+</h3>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</body>
+</html>
